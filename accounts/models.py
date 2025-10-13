212e98c1f39575a86f11523f48c9330cf9cb3630
RaufAkbar <raufakbar324@gmail.com>
2025-10-13 22:26:39 +0500

    Replace old project with new project

--- accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    email = models.EmailField(blank=True, null=True)
-    stripe_customer_id = models.CharField(max_length=128, null=True, blank=True)
-
-    REQUIRED_FIELDS = []  # keep default field set happy
--- billing/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsSubscriber(BasePermission):
-    def has_permission(self, request, view):
-        ctx = request.auth  # set by ApiKeyAuthentication
-        return bool(ctx) and ctx.get("plan") in {"pro", "team"}
